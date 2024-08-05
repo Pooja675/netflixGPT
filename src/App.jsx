a7fd5c181183fcc548615ebc2e8dbf71d0279d66
Pooja675 <poojasinha675@gmail.com>
2024-08-05 11:40:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-green-700'>Hello I am MERN full stack developer.</h1>
+    </>
+  )
+}
+
+export default App
